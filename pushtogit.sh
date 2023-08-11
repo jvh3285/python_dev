2f73a4e1d5ae6d5793c99e70de0ae4a461f0bb76
root <jvh3285@gmail.com>
2023-08-11 12:50:59 +0900

    2023-08-11 12:50:59

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,9 +1,29 @@
-!/bin/bash
-# lines that start like this are shell comments
-# read projects current directory with $PWD
-echo "running command from" $PWD
-cd $PWD
+#!/bin/bash
+
+# get current branch and push
+current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+# git pull
+git pull origin "$current_branch"
+echo "====pull changes from '$current_branch' branch"
+
+# get the argument message
+message="$1"
+
+# If no commit message is passed, use current date time in the commit message
+if [[ -z "${message// }" ]]
+    then
+        message=$(date '+%Y-%m-%d %H:%M:%S')
+fi
+
+# stage all changes
 git add .
-echo "Enter commit message: "
-git commit -am "$commitMessage"
-git push
+echo "====staged all git files"
+
+# add commit
+git commit -m "$message"
+echo "====added the commit with message: '$message'"
+
+# git push
+git push origin "$current_branch"
+echo "====pushed changes to '$current_branch' branch"
