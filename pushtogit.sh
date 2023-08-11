4fb55d5eb9d39813c73099188e5b360b8a49b741
root <jvh3285@gmail.com>
2023-08-11 13:40:44 +0900

    Fri Aug 11 13:40:44 KST 2023 commit

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
+source ~/.bashrc
 
-# get current branch and push
-current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+date=$(date)
+github_id=$env_github_id
+github_Token=$env_github_Token
+github_Address=$env_autoTrade_git
+## 변경
+Project_name="python_dev"
+## 변경
+logFile="/container/git/"$Project_name"_push.log"
+export SourceDir="/container/git/Source"
 
-# git pull
-git pull origin "$current_branch"
-echo "====pull changes from '$current_branch' branch"
+cd $SourceDir
+sleep 0.1
 
-# get the argument message
-message="$1"
+echo "========= push Start (Date : $date) =========" && echo "========= push Start (Date : $date) =========" >$logFile 2>&1
+echo "git add . ..." && echo "git add . ..." >>$logFile 2>&1
+echo "$(git add .)" >>$logFile 2>&1
+echo "" && echo "" >>$logFile 2>&1 && echo "==" >>$logFile 2>&1
 
-# If no commit message is passed, use current date time in the commit message
-if [[ -z "${message// }" ]]
-    then
-        message=$(date '+%Y-%m-%d %H:%M:%S')
+echo "git status" && echo "git status" >>$logFile 2>&1
+echo "$(git status)" >>$logFile 2>&1
+echo "" && echo "" >>$logFile 2>&1 && echo "==" >>$logFile 2>&1
+
+# Choose Commit Type [#
+source $SourceDir/Choose_Commit_Type.sh
+sleep 0.1
+# Choose Commit Type ]#
+echo ""
+# Enter Commit Title [#
+printf "Enter Commit Title : "
+read Commit_Title
+Commit_Msg="$env_CType $Commit_Title"
+# Enter Commit Title ]#
+
+echo "git commit -m $Commit_Msg" && echo "git commit -m $Commit_Msg" >>$logFile 2>&1
+echo "$(git commit -m "$Commit_Msg")" >>$logFile 2>&1
+echo "" && echo "" >>$logFile 2>&1 && echo "==" >>$logFile 2>&1
+
+echo "git push!" && echo "git push!" >>$logFile 2>&1
+git push https://$github_id:$github_Token@$github_Address >>$logFile 2>&1
+
+sleep 2
+
+result="$(awk '/Everything up-to-date/' $logFile 2>&1)"
+echo "$result"
+
+if [ -z "$result" ]; then
+  echo "git push Success~!" && echo "git push Success~!" >>$logFile 2>&1
+  echo "========= push OK (Date : $date) =========" && echo "========= push OK (Date : $date) =========" >>$logFile 2>&1
+else
+  echo "!! git push ERROR! please check $logFile !!" && echo "!! git push ERROR! please check $logFile !!" >>$logFile 2>&1
 fi
 
-# stage all changes
-git add .
-echo "====staged all git files"
-
-# add commit
-git commit -m "$message"
-echo "====added the commit with message: '$message'"
-
-# git push
-git push origin "$current_branch"
-echo "====pushed changes to '$current_branch' branch"
+exit 0
